0f4cd0629c475e965a06fd8b1d743f0c2763f0ae
jayeebee <pnuthexsa311@gmail.com>
2021-04-17 21:59:34 -0400

    semi final notes

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,4 @@
-
-
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//ctrl+shift+v to preview
 
 function generateMarkdown(data) {
   return `
@@ -42,7 +30,7 @@ function generateMarkdown(data) {
   ${data.tests}
   ## Licensing
   ${data.licenseInfo}
-  
+
   ## Any Questions? Contact me!
   ### [Github](https://github.com/${data.github})
   
